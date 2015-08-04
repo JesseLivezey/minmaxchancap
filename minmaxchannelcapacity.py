90009cebd08de46a3cee6ed69ea51ee801a3a9b4
a-krishnan7 <iyeramrith@gmail.com>
2015-08-04 14:45:59 -0700

    rewritten with numpy arrays and list selections

--- minmaxchannelcapacity.py
+++ minmaxchannelcapacity.py
@@ -6,35 +6,72 @@
 
 from matplotlib import pyplot as plt
 import numpy as np
-import scipy as sp
+from scipy import optimize
 import math
 
+#initialize variables and numpy arrays
 j = 0
 i = 0
 c = 0
 n = 3
-Pyhy = 0
-Pyj = float(1/n)
-Pyhi = 0
+r = 0
+Pyhy = np.zeros((n,n))
+Pyh = np.zeros((n,1))
+Py = np.zeros((n,1))
 
-def calcPyhi(n, Pyhy, Pyj):
-    if j <= n:
+Py[j] = 1/n
+
+#Calculate Pyh[i] given n, Pyhy, and Py using the formula sigma(index j) P(Yhi|Yj)*P(Yj)
+def calcPyh(n, Pyhy, Py):
+    for i in range(n):
         for j in range(n):
-            Pyhi = float(Pyhy)*float(Pyj)
-            return Pyhi
-        
+            Pyh[i] = Pyh[i] + Py[j]*Pyhy[i,j]
+            return Pyh[i]
         j = j+1
-        
-    else:
-        pass
-    
-def f(Pyhy, Pyj, Pyhi):
-    c = Pyhy*Pyj*math.log(float(Pyhy/Pyhi), 2)
+    i = i + 1
+    return Pyh
+
+#calculate c given Pyhy, Py, Pyh
+def f(Pyhy, Py, Pyh):
+    c = Pyhy[i,j]*Py[j]*math.log(Pyhy[i,j]/Pyh[i], 2)
     return c
-    
+
+#calculate the capacity with a summation with a nested "for" loop inside another "for" loop
+c1 = 0
 def cap(Pyhy):
     for i in range(n):
         for j in range(n):
+            c1 = c + f(Pyhy, Py, Pyh)
+            if Pyh[i] > 0: 
+                return c
+            else:
+                pass
+            j = j + 1
+        i = i + 1
+    return c
+
+
+#Set the constraints on the function "cap"
+cons = ({'type': 'eq',
+          'fun' : lambda x: np.array(n*Py - 1),
+          },
+          
+        {'type': 'ineq',
+          'fun' : lambda x: np.array([]),
+          },
+          
+        {'type': 'ineq',
+          'fun' : lambda x: np.array([]),
+          })
+          
+result = optimize.minimize(cap, [],
+         constraints=cons, method='SLSQP', options={'disp': True})
+        
+print result
+        
+            
+            
+            
             
         
         
