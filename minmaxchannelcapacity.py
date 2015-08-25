357abc4b2649a977c938cf11a0f27f669d71ba25
a-krishnan7 <iyeramrith@gmail.com>
2015-08-24 23:27:08 -0700

    Test added
    Test Function
    
    “Singular Matrix C in LSQ Subproblem”
    Terminating optimization

--- minmaxchannelcapacity.py
+++ minmaxchannelcapacity.py
@@ -15,13 +15,15 @@ import math
 n = 3
 #the following arrays are arrays used to test the function
 Pyhy = np.array([[0.2, 0.4, 0.4], [0.5, 0.4, 0.1], [0.3, 0.4, 0.3]])
-Pyh = np.random.rand(n,1)
-Pyh /= Pyh.sum()
+Pyh = np.random.rand(n,1.0)
+Pyhyguess = np.ones((n,n))
 #initialize the P(Yj) array 
 x = 1.0/float(n)
-Py = x*np.ones(n)
+Py = x*np.ones(float(n))
 #r is the fixed classification accuracy
 r = 0.40
+#flatten the array
+Pyhy_i=np.ravel(Pyhy, order='C')
                 
 #alternatively, the arrays can be configured into arrays of zeros using
 '''Pyhy = np.zeros((n,n))
@@ -39,64 +41,60 @@ plt.imshow(Py, interpolation= 'nearest')
 plt.show()'''
 
 #Calculate Pyh[i] given n, Pyhy, and Py using the formula sigma(index j) P(Yhi|Yj)*P(Yj)
-def calcPyh(n, Pyhy, Py):
+def calcPyh(inputArray):
+    Pyh_i = np.zeros(n)
+    Pyhy_i = inputArray.reshape(n,n)
     for i in range(n):
         for j in range(n):
-            Pyh[i] = Pyh[i] + Py[j]*Pyhy[i,j]
-    return Pyh
+            Pyh_i[i] = Pyh_i[i] + Py[j]*Pyhy_i[i,j]
+    return Pyh_i
 
 #plt.imshow(Pyh, interpolation='nearest')
 #plt.show()
 
 #calculate c given Pyhy, Py, Pyh using the formula for channel capacity (without the supremum)
 def chCapMin(inputArray):
-    chCapMin1 = 0
+    Pyhy_i = inputArray.reshape(n,n)
+    chCapMin1 = 0.0
+    Pyh_ii = calcPyh(Pyhy_i)
     for i in range(n):
         for j in range(n):
-            inParenPrimer = float(Pyhy[i,j])/float(Pyh[i])
-            inParen = math.log(inParenPrimer, 2)
-            a = float(Pyhy[i,j])
-            b = float(Py[j])
-            d = float(inParen)
-            c = a*b*d
-            chCapMin1 = chCapMin1 + c
+            inParenPrimer = float(Pyhy[i,j])/float(Pyh_ii[i])
+            if inParenPrimer > 0:
+                inParen = math.log(inParenPrimer, 2)
+                a = float(Pyhy[i,j])
+                b = float(Py[j])
+                d = float(inParen)
+                c = a*b*d
+                chCapMin1 = chCapMin1 + c
     return chCapMin1
 
 def chCapMax(inputArray):
-    chCapMax1 = 0
-    for i in range(n):
-        for j in range(n):
-            inParenPrimer = float(Pyhy[i,j])/float(Pyh[i])
-            inParen = math.log(inParenPrimer, 2)
-            a = float(Pyhy[i,j])
-            b = float(Py[j])
-            d = float(inParen)
-            c = -(a*b*d)
-            chCapMax1 = chCapMax1 + c
-    return chCapMax1
+    return -chCapMin(inputArray)
 
 #constraint 1: Pyhy > 0
 def con1(inputArray):
-    return Pyhy.min()
+    return inputArray.min()
 
 #constraint 2: sum of Pyhy elements in a row = 1 because total probability is 1
 #removed as of right now because it is exiting function - "more equality constraints than independent variables"
 def con2(inputArray):
-    for i in range(n):
-        con2a = Pyhy[i].sum() - 1
-        con2b = math.pow(con2a, 2)
-    return con2b
+    Pyhy_i = inputArray.reshape(n,n)
+    con2a = ((Pyhy_i.sum(axis=1)-1)**2).min() 
+    return con2a
 
 #constraint 3: fixed classification accuracy, summation(index i) of Pyhy * Pyi = r
 def con3(inputArray):
+    inProduct = 0
+    Pyhy_i = inputArray.reshape(n,n)
     for i in range(n):
-        for j in range(n):
-            inProduct = Pyhy[i,j]*Py[j]
+        inProduct += Pyhy_i[i,i]*Py[i]
     con3a = inProduct - r 
-    return con3a.min()
+    return con3a
 
 #this alternative 3rd constraint uses numpy.sum instead of a for loop within another for loop.
 def alternativecon3(inputArray):
+    Pyhy = inputArray.reshape(n,n)
     axisSum = np.sum(Pyhy, axis=1)
     inSummation = axisSum*Py
     con3z = inSummation - r
@@ -104,33 +102,50 @@ def alternativecon3(inputArray):
 
 #this is the constraint that applies to rows where there are only 0s and 1s?
 def con4(inputArray):
+    Pyhy_i = inputArray.reshape(n,n)
     for i in range(n):
         for j in range(n):
-            zeros = Pyhy[i,j] - Pyhy[i,j-1]
+            zeros = Pyhy_i[i,j] - Pyhy_i[i,j-1]
             return zeros - 1
 
 #Set the constraints on the function "cap"
 cons = ({'type': 'ineq', 'fun' : con1},
-#        {'type': 'eq', 'fun' : con2},
+        {'type': 'eq', 'fun' : con2},
         {'type': 'eq', 'fun' : con3})
           
 #use an optimization to minimize the function in the form "minimize(funcName, [guess], constraints=, method=, options= )
+#may have to add additional arguments to maximize
 
-#flatten the array
-np.ravel(Pyhy, order='C')
-
-#the following two optimizations minimize the function by minimizing the ChCap function (ChCapMin)
+#the following two optimizations minimize the function by minimizing the ChCap function
           
-minimizePyhy = optimize.minimize(chCapMin, [1], constraints=cons, method='SLSQP', options={'disp': True})
-print minimizePyhy
+minimizePyhy = optimize.minimize(chCapMin, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
+#print minimizePyhy
 
-minimizePyhyfmin = optimize.fmin_slsqp(chCapMin, [1], eqcons=[con3], ieqcons=[con1])
-print minimizePyhyfmin
+#minimizePyhyfmin = optimize.fmin_slsqp(chCapMin, Pyhyguess, eqcons=[con3,con2], ieqcons=[con1])
+#print minimizePyhyfmin
 
-#the following two optimizations maximize the function by minimizing the negative of the ChCap function (ChCapMax)
+#the following two optimizations maximize the function by minimizing the negative of the ChCap function
 
-maximizePyhy = optimize.minimize(chCapMax, [1], constraints=cons, method='SLSQP', options={'disp': True})
-print maximizePyhy
+maximizePyhy = optimize.minimize(chCapMax, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
+#print maximizePyhy
 
-maximizePyhyfmin = optimize.fmin_slsqp(chCapMax, [1], eqcons=[con3], ieqcons=[con1]) 
-print maximizePyhyfmin
+#maximizePyhyfmin = optimize.fmin_slsqp(chCapMax, Pyhyguess, eqcons=[con3,con2], ieqcons=[con1]) 
+#print maximizePyhyfmin
+
+#the following are functions used to test whether the chcap function works
+
+def testfunc(inputArray):
+    Pyhy_itest = np.array([[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]])
+    Py_itest = np.array([[1.0/float(n)],[1.0/float(n)],[1.0/float(n)]])
+    Pyh_itest = calcPyh(Pyhy_itest)
+    chCapMin_itest = chCapMin(Pyhy_itest)
+    return chCapMin_itest
+
+minimizePyhyTestminimize = optimize.minimize(testfunc, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
+print minimizePyhyTestminimize
+
+minimizePyhyTestfmin = optimize.fmin_slsqp(testfunc, Pyhyguess, eqcons=[con3,con2], ieqcons=[con1])
+print minimizePyhyTestfmin
+
+
+
