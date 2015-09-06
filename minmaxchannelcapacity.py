6cdc7f6015799ce95201640df548dea1137ace99
a-krishnan7 <iyeramrith@gmail.com>
2015-09-06 16:10:15 -0700

    Updated Code
    Plots with color bar added.
    Third test added.

--- minmaxchannelcapacity.py
+++ minmaxchannelcapacity.py
@@ -4,6 +4,7 @@
 #import libraries
 from matplotlib import pyplot as plt
 import numpy as np
+import numpy.matlib
 from scipy import optimize
 import math
 
@@ -24,20 +25,14 @@ Py = x*np.ones(float(n))
 r = 0.40
 #flatten the array
 Pyhy_i=np.ravel(Pyhy, order='C')
-                
-#alternatively, the arrays can be configured into arrays of zeros using
-'''Pyhy = np.zeros((n,n))
-Pyh = np.zeros((n,1))
-Py = np.zeros((n,1))'''
 
 #plot show is used to test whether the arrays are in a desirable format
 '''plt.imshow(Pyhy, interpolation= 'nearest')
+plt.colorbar()
 plt.show()
 
 plt.imshow(Pyh, interpolation= 'nearest')
-plt.show()
-
-plt.imshow(Py, interpolation= 'nearest')
+plt.colorbar()
 plt.show()'''
 
 #Calculate Pyh[i] given n, Pyhy, and Py using the formula sigma(index j) P(Yhi|Yj)*P(Yj)
@@ -48,9 +43,6 @@ def calcPyh(inputArray):
         for j in range(n):
             Pyh_i[i] = Pyh_i[i] + Py[j]*Pyhy_i[i,j]
     return Pyh_i
-
-#plt.imshow(Pyh, interpolation='nearest')
-#plt.show()
 
 #calculate c given Pyhy, Py, Pyh using the formula for channel capacity (without the supremum)
 def chCapMin(inputArray):
@@ -76,11 +68,11 @@ def chCapMax(inputArray):
 def con1(inputArray):
     return inputArray.min()
 
-#constraint 2: sum of Pyhy elements in a row = 1 because total probability is 1
+#constraint 2: sum of Pyhy elements in the matrix = 1*n because total probability is 1 added n times
 def con2(inputArray):
     Pyhy_i = inputArray.reshape(n,n)
     inputArraySum = np.sum(Pyhy_i)
-    con2a = inputArraySum - (n**2)
+    con2a = inputArraySum - (n)
 #    for j in range(n-1):
 #        axisSum = Pyhy_i.sum(axis=j)-1
 #        con2a = (axisSum**2).min()
@@ -104,12 +96,14 @@ def alternativecon3(inputArray):
     con3z = inSummation - r
     return con3z
 
-#this is the constraint that applies to rows where there are only 0s and 1s?
+#this is the constraint that applies to rows where there are only 0s and 1s
 def con4(inputArray):
     Pyhy_i = inputArray.reshape(n,n)
+    zeros=0
     for i in range(n):
         for j in range(n):
-            zeros = Pyhy_i[i,j] - Pyhy_i[i,j-1]
+            if Pyhy_i[i,j] == 1.0:                
+                zeros = Pyhy_i[i,j] - Pyhy_i[i,j-1]
     con4a = zeros - 1.0
     return con4a
 
@@ -120,22 +114,21 @@ cons = ({'type': 'ineq', 'fun' : con1},
         {'type': 'ineq', 'fun' : con4})
           
 #use an optimization to minimize the function in the form "minimize(funcName, [guess], constraints=, method=, options= )
-#may have to add additional arguments to maximize
 
 #the following optimization minimizes the function
           
-minimizePyhy = optimize.minimize(chCapMin, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
+#minimizePyhy = optimize.minimize(chCapMin, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
 #print minimizePyhy
 
-minimizePyhyfmin = optimize.fmin_slsqp(chCapMin, Pyhyguess, eqcons=[con3,con2], ieqcons=[con1,con4])
+minimizePyhyfmin = optimize.fmin_slsqp(chCapMin, Pyhyguess, eqcons=[con3], ieqcons=[con1,con4])
 #print minimizePyhyfmin
 
 #the following optimization maximizes the function by minimizing the negative of the function
 
-maximizePyhy = optimize.minimize(chCapMax, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
+#maximizePyhy = optimize.minimize(chCapMax, Pyhyguess, constraints=cons, method='SLSQP', options={'disp': True})
 #print maximizePyhy
 
-maximizePyhyfmin = optimize.fmin_slsqp(chCapMax, Pyhyguess, eqcons=[con3,con2], ieqcons=[con1,con4])
+maximizePyhyfmin = optimize.fmin_slsqp(chCapMax, Pyhyguess, eqcons=[con3], ieqcons=[con1,con4])
 #print minimizePyhyfmin
 
 #the following are functions used to test whether the chcap function works
@@ -144,23 +137,42 @@ maximizePyhyfmin = optimize.fmin_slsqp(chCapMax, Pyhyguess, eqcons=[con3,con2], 
 #an inputArray where the diagonal is all ones and everything else is 0 yields a channel capacity of logbase2(n)
 
 def chCapTestFunc():
-    Pyhy_test = np.array([[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]])
+    Py = x*np.ones(float(n))
+    Pyhy_test = np.matlib.identity(n, dtype='float')
     chCapTest = 0.0
     Pyhy_testi=np.ravel(Pyhy_test, order='C')
     chCapTest = chCapMin(Pyhy_testi)
     assert np.allclose(np.log2(n), chCapTest) # - - It Works!!
     return chCapTest
 
+chCapTestFunc()
+
 #the following function makes sure that for a 2darray of n x n dimensions...
 #an inputArray where every item is filled with ones yields a channel capacity of 0
 
 def chCapTestFunc2():
-    Pyhy_test2 = np.array([[1.0,1.0,1.0],[1.0,1.0,1.0],[1.0,1.0,1.0]])
+    Py = x*np.ones(float(n))
+    Pyhy_test2 = np.ones((n,n), dtype='float')/n
     chCapTest2 = 0.0
     Pyhy_test2i=np.ravel(Pyhy_test2, order='C')
     chCapTest2 = chCapMin(Pyhy_test2i)
     assert np.allclose(0.0, chCapTest2) # - - It Works!!
     return chCapTest2
+
+chCapTestFunc2()
+
+#This is the case where you are only inputting one type of class and so you still transmit no information.
+
+def chCapTestFunc3():
+    Py = np.zeros(n)
+    Py[0] = 1.0
+    Pyhy_test3 = np.ones((n,n), dtype='float')/n
+    chCapTest3 = 0.0
+    Pyhy_test3i=np.ravel(Pyhy_test3, order='C')
+    chCapTest3 = chCapMin(Pyhy_test3i)
+    assert np.allclose(0.0, chCapTest3)
+    
+chCapTestFunc3()
 
 
 
